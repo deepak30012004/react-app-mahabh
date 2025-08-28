ca40c9c049f826e15b2c3ea714ce1067acd394c9
deepak singh <deepaksingh30012004@gmail.com>
2025-08-28 15:13:50 +0530

    Your commit message

--- server/src/app.js
+++ server/src/app.js
@@ -20,6 +20,44 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+
+const redis = require('redis');
+const client = redis.createClient();
+
+client.connect().then(() => {
+    console.log('Connected to Redis');
+}).catch((err) => console.log('Redis Connection Error:', err));
+
+const cacheMiddleware = (keyGenerator) => {
+  return async (req, res, next) => {
+    try {
+      const key = keyGenerator(req); // Generate a key for this request
+      const cachedData = await client.get(key);
+
+      if (cachedData) {
+        // If cache exists, return it
+        return res.json(JSON.parse(cachedData));
+      } else {
+        // Override res.json to store response in Redis
+        res.sendResponse = res.json;
+        res.json = (body) => {
+          client.setEx(key, 60, JSON.stringify(body)); // Cache for 60s
+          res.sendResponse(body);
+        };
+        next();
+      }
+    } catch (err) {
+      console.error(err);
+      next();
+    }
+  };
+};
+
+
+
+
+
 // healthcheck
 app.get("/", (req, res) => res.status(200).send("Ok 🎃"))
 // User routes
@@ -30,7 +68,26 @@ app.post("/user/refresh", callback(RefreshToken))
 app.post("/key/create", applyMiddleware(auth, callback(CreateKey)))
 app.delete("/key/delete", applyMiddleware(auth, callback(DeleteKey)))
 // Character routes
-app.get("/character/info", callback(CharacterInfo))
+// app.get("/character/info", callback(CharacterInfo))
+
+
+// Assuming you already have `client` as your Redis client and cacheMiddleware in app.js
+
+// Updated route with cache middleware
+app.get(
+  "/character/info",
+  cacheMiddleware(() => "character_info"), // Redis key for this route
+  async (req, res) => {
+    try {
+      const data = await CharacterInfo(); // your existing callback function
+      res.json(data);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  }
+);
+
 
 function applyMiddleware(middleware, callback) {
   return (req, res, next) => {
